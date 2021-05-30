26dfa6483b60c0613a9ec221532cc4c91f200cab
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-05-30 17:42:01 +0700

    lab2

---
+++ Lab02/Lab02/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02.Models
+{
+    class User
+    {
+        private string name;
+        private string email;
+        private string content;
+
+
+        public User(string name, string email, string content)
+        {
+            Name = name;
+            Email = email;
+            Content = content;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+
+    }
+}
