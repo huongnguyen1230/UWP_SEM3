5cfb40451204a80054662f2d3f907d0b9899e824
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-06-14 21:01:32 +0700

    huong

---
+++ PRACTICE/PRACTICE/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PRACTICE.Model
+{
+        public class Employee
+        {
+            public string Name { get; set; }
+            public string Role { get; set; }
+            public int Birthyear { get; set; }
+        }
+    public class Root
+    {
+        public List<Employee> EmployeeLists { get; set; }
+    }
+}
